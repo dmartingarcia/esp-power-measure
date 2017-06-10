afa9e9fc634676d38f78db5fcf6a848dc6859318
David Martin Garcia <base.dks@gmail.com>
2017-06-10 12:27:29 +0200

    refactoring influx client part

--- powermetter.ino
+++ powermetter.ino
@@ -8,15 +8,17 @@
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
-
-
+#include <ESP8266HTTPClient.h>
+#define VERSION 1
+#define SENS 0.1875
 #define AC_FREQ 50
-#define SAMPLES 500
+#define SAMPLES 100
 
 Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
 
 const int sclPin = D2;
 const int sdaPin = D1;
+char buffer[100];
 
 void setup()
 {
@@ -28,14 +30,14 @@ void setup()
   oled.clearDisplay();              // Clear screen
   oled.setTextXY(0,0);              // Set cursor position, start of line 0
   oled.putString("INIT");
-  if (!wifiManager.autoConnect("ESP")) {
+  if (!wifiManager.autoConnect("ESP-watts")) {
     Serial.println("failed to connect, we should reset as see if it connects");
     delay(3000);
     ESP.reset();
     delay(5000);
   }
   
-  ads.setGain(GAIN_ONE);  // 0.125mV (default)
+  ads.setGain(GAIN_TWOTHIRDS);  // 0.1875mV (default)
   ads.begin();
   ArduinoOTA.setHostname("esp-watts");
   ArduinoOTA.begin();
@@ -49,18 +51,19 @@ void loop() {
   ArduinoOTA.handle(); 
 
   int16_t adc0 = 0;
-  float volts = 0;
+  float volts_max = 0.0;
 
   for (int i = 0; i < SAMPLES; i++) {
-    volts =  _max(volts, ads.readADC_SingleEnded(3));
+    adc0 = ads.readADC_SingleEnded(3);
+    volts_max =  _max(volts_max, adc0);
   }
    
-  volts = volts * 0.125 / 1000; 
-  
-  float vcc = ads.readADC_SingleEnded(0) * 0.125 / 1000;
-  float offset = vcc / 2 + 0.03;
-  float diff_volts = volts - offset;
-  float current = abs(diff_volts) / 0.066;
+  volts_max = volts_max * SENS / 1000; 
+  //volts_min = volts_min * SENS / 1000;
+  float vcc = ads.readADC_SingleEnded(0) * SENS / 1000;
+  float offset = 2.5; /*vcc / 2 + 0.03;*/
+  float diff_volts = abs(volts_max - offset);
+  float current = diff_volts / 0.066;
   float currentRMS = 0.707 * current;
   float power = 230.0 * currentRMS;
 
@@ -80,8 +83,38 @@ void loop() {
   oled.putString("Watt: ");
   oled.putFloat(power);
   oled.putString("        ");
-  oled.setTextXY(5,0);             // Set cursor position, line 2 10th character
+  oled.setTextXY(4,0);             // Set cursor position, line 2 10th character
   oled.putString("Offs: ");
   oled.putFloat(offset);
   oled.putString("        ");
+  oled.setTextXY(5,0);             // Set cursor position, line 2 10th character
+  oled.putString("Vread: ");
+  oled.putFloat(volts_max);
+  
+  char power_string[10];
+  add_headers();
+  add_measurement("consumption", power);
+  add_measurement("vcc", vcc);
+  add_measurement("current", current);
+  add_measurement("vread", volts_max);
+  oled.setTextXY(6,0);
+  oled.putString("HTTP req: ");
+  oled.putNumber(post_measurements());
 }
+
+void add_headers(){
+  sprintf(buffer, "wattmetter,host=%d version=%d", ESP.getChipId(), VERSION);
+}
+
+void add_measurement(char* name, float measurement){
+  char measurement_buffer[10];
+  dtostrf(measurement, 4, 6, measurement_buffer);
+  sprintf(buffer, "%s,%s=%s", buffer, name, measurement_buffer);
+}
+
+int post_measurements(){
+  HTTPClient http;
+  http.begin("http://192.168.1.134:8086/write?db=sensors");
+  return http.POST(buffer);
+}
+
